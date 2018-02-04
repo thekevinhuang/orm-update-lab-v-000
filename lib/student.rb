fa7e5573e64651a365db54f28801e674b6c88ab2
Kevin Huang <yellow.khaos@gmail.com>
2018-02-04 20:47:49 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -65,7 +65,13 @@ class Student
     new_student = Student.new(row[1],row[2],row[0])
   end
 
-  def self.find_by_name
+  def self.find_by_name(name)
+    sql = <<-SQL
+      SELECT * FROM students WHERE
+      name = ?
+      SQL
+
+    self.new_from_db(DB[:conn].execute(sql, name))
   end
 
 end
