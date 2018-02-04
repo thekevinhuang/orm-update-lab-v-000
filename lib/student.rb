4e1dfc277e84954b713389a5a046be01ac044389
Kevin Huang <yellow.khaos@gmail.com>
2018-02-04 20:34:01 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -59,11 +59,10 @@ class Student
     new_student = Student.new(name, grade)
     new_student.save
     new_student
-end
+  end
 
   def self.new_from_db(row)
     new_student = Student.new(row[1],row[2],row[0])
   end
 
   def self.find_by_name
-
